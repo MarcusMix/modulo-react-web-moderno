57c40774bff558652136ddf4fc7dcddced0a5d0d
MarcusMix <tradedomarcus@gmail.com>
2022-08-19 15:58:01 -0300

    ex react

--- react/exercicios/src/index.js
+++ react/exercicios/src/index.js
@@ -2,7 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import Primeiro from './componentes/Primeiro'
+import BomDia from './componentes/BomDia'
+
+// import  BoaTarde, {  BoaNoite } from './componentes/Multiplos'
+import  Multi from './componentes/Multiplos'
 
 // const elemento = <h1>React</h1>
 
-ReactDOM.render(<Primeiro/>, document.getElementById('root'))
+// ReactDOM.render(<Primeiro/>, document.getElementById('root'))
+ReactDOM.render(
+        <div>
+            <Multi.BoaNoite nome=" Marcus"/>
+            <Multi.BoaTarde nome=" marcus"/>
+        </div>
+    , document.getElementById('root'))
