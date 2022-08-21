fe75aa21f02b74c85d0567c89eb2d57fe906c646
MarcusMix <tradedomarcus@gmail.com>
2022-08-21 19:24:08 -0300

    exemplos react

--- react/exercicios/src/index.js
+++ react/exercicios/src/index.js
@@ -1,18 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import Primeiro from './componentes/Primeiro'
-import BomDia from './componentes/BomDia'
+// import Primeiro from './componentes/Primeiro'
+// import BomDia from './componentes/BomDia'
 
 // import  BoaTarde, {  BoaNoite } from './componentes/Multiplos'
-import  Multi from './componentes/Multiplos'
+// import  Multi from './componentes/Multiplos'
+import  Saudacao from './componentes/Saudacao'
 
 // const elemento = <h1>React</h1>
 
 // ReactDOM.render(<Primeiro/>, document.getElementById('root'))
 ReactDOM.render(
         <div>
-            <Multi.BoaNoite nome=" Marcus"/>
-            <Multi.BoaTarde nome=" marcus"/>
+            {/* <Multi.BoaNoite nome=" Marcus"/>
+            <Multi.BoaTarde nome=" marcus"/> */}
+            <Saudacao tipo="bom dia" nome="Marcus"/>
         </div>
     , document.getElementById('root'))
