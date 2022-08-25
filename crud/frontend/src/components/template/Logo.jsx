4dd7432e1337decf1bba835ef301c4946e82e237
MarcusMix <tradedomarcus@gmail.com>
2022-08-25 15:36:17 -0300

    update logo

--- crud/frontend/src/components/template/Logo.jsx
+++ crud/frontend/src/components/template/Logo.jsx
@@ -1,7 +1,10 @@
 import './Logo.css'
+import logo from '../../assets/images/CC.png'
 import React from 'react'
 
 export default props =>
     <aside className='logo'>
-        logo
+        <a href="#" className='logo'>
+            <img src={logo} alt="logo" />
+        </a>
     </aside>
